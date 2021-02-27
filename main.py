547b66ba8a522bd2106bd2f8679a3db30c530741
Hakun <stankovskia@gmail.com>
2021-02-27 10:16:20 +0100

    Whole Project
    # If you want to train your model, add a new folder in the images folder and name it with the desired label name and then add your images in that folder

---
+++ main.py
@@ -0,0 +1,42 @@
+import cv2
+import pickle
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v: k for k, v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while (True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 50:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
+        img_item = "my_image.png"
+        cv2.imwrite(img_item, roi_gray)
+        color = (0, 0,255)
+        if labels[id_] == 'alek':
+            color = (255, 0, 0)
+        stroke = 2
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, stroke)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
